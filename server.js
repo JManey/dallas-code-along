b95ce8f71468c19963b728b3fbb5f7063f71ccc2
JMoney-216 <jdm216@gmail.com>
2019-11-13 11:05:32 -0600

    updated index.ejs and server.js

--- server.js
+++ server.js
@@ -2,10 +2,22 @@
 const express = require('express');
 const app = express();
 
+// configure express app
+app.set('view engine', 'ejs');
+
+
 // port for listener
 const port = 3000;
 
+const user = "jmoney";
+
+// setup a route that resonds to requests to the root route
+// 'http://localhost:3000'
+app.get('/', function(req, res) {
+    res.render('index', {user});
+});
 //add listener
 app.listen(port, () => {
     console.log(`Express is listening on port ${port}`);
 });
+
